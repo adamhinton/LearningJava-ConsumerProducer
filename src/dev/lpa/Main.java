b21a4f050f6d1624b8ac25eed10fc438a8e365b2
adamd <adam.d.hinton@gmail.com>
2024-04-28 11:33:38 -0400

    my first Consumer and Producer

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,5 +1,8 @@
 package dev.lpa;
 
+import java.util.Random;
+
+// Consumer
 class MessageRepository{
 
     private String message;
@@ -29,6 +32,42 @@ class MessageRepository{
 
         hasMessage = true;
         this.message = message;
+    }
+}
+
+// Producer
+class MessageWriter implements Runnable{
+
+    private MessageRepository outgoingMessage;
+
+    private final String text = """
+            Humpty Dumpty sat on a wall,
+            Humpty Dumpty had a great fall,
+            All the king's horses and all the king's men,
+            Couldn't put Humpty together again.""";
+
+    public MessageWriter(MessageRepository outgoingMessage) {
+        this.outgoingMessage = outgoingMessage;
+    }
+
+    @Override
+    public void run() {
+
+        Random random = new Random();
+
+        String[] lines = text.split("\n");
+
+        for (int i=0; i< lines.length; i++){
+            outgoingMessage.write(lines[i]);
+            try{
+                Thread.sleep(random.nextInt(500, 2000));
+            }
+            catch (InterruptedException e){
+                throw new RuntimeException(e);
+            }
+        }
+        outgoingMessage.write("Finished");
+
     }
 }
 
